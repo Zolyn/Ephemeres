ce3839cb92fa357acbee27d2b47a361843690a0c
Zorin <zorin1477@gmail.com>
2022-02-11 02:32:47 +0800

    test: scroll behavior

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@
 import { createMemoryHistory, createRouter as _createRouter, createWebHistory } from 'vue-router';
 import { setupLayouts } from 'layouts-generated';
 import pageRoutes from '~pages';
+import emitter from '~/eventbus';
 
 const routes = setupLayouts(pageRoutes);
 
@@ -11,5 +12,14 @@ export function createRouter() {
         // import.meta.env.SSR is injected by Vite.
         history: import.meta.env.SSR ? createMemoryHistory() : createWebHistory(),
         routes,
+        scrollBehavior() {
+            // TODO: implement savedPosition to replace the same functionality of Vue Router
+            return new Promise((resolve) => {
+                setTimeout(() => {
+                    emitter.emit('scroll', { top: 0, behavior: 'smooth' });
+                    resolve();
+                }, 700);
+            });
+        },
     });
 }
