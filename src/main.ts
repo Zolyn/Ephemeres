4a08667758e743d572493de5ff726be7fa3d3ea2
Zorin <zorin1477@gmail.com>
2022-03-11 22:51:00 +0800

    feat: unocss

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createSSRApp, createApp as createClientApp } from 'vue';
 import { createHead } from '@vueuse/head';
 import App from './App.vue';
 import { createRouter } from './router';
+import 'uno.css';
 
 // SSR requires a fresh app instance per request, therefore we export a function
 // that creates a fresh app instance. If using Vuex, we'd also be creating a
@@ -12,7 +13,7 @@ export function createApp() {
     const head = createHead();
     const pinia = createPinia();
     app.use(router);
-    // NOTE: I don't know whether it is necessary to hydrate the state manually
+    // NOTE: I don't know whether it is necessary to hydrate the initial state manually
     //  even though this project doesn't require data prefetching :(
     app.use(pinia);
     return { app, router, head };
