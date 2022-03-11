944aab405006ecb23947bfc80a0c0748ee4fe21d
Zorin <zorin1477@gmail.com>
2022-03-11 22:56:00 +0800

    feat: project config

---
+++ src/configs/ephemeres.ts
@@ -0,0 +1,9 @@
+// NOTE: DO NOT DELETE THIS FILE
+import { createConfig } from '~/utils';
+
+export default createConfig({
+    backend: 'http://localhost:3001',
+    providerUrls: {
+        upyun: 'https://repo-zorin.beaa.cn',
+    },
+});
