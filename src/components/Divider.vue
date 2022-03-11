f313f1f3a6f9732633d7361fae66602c078baeb1
Zorin <zorin1477@gmail.com>
2022-03-11 22:55:40 +0800

    feat(components): divider

---
+++ src/components/Divider.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="divider hidden md:(flex items-center)">
+        <n-icon size="16" :component="DividerTall16Filled" />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { DividerTall16Filled } from '@vicons/fluent';
+</script>
